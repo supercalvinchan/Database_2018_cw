a9f60b49a2e0e10b8eff710dd83e155979336bc1
calvinchan <calvinchan@dyn1209-40.wlan.ic.ac.uk>
2018-02-02 18:12:17 +0000

    databasecoursework

---
+++ db_2018_cw1_template.sql
@@ -0,0 +1,33 @@
+
+-- Q1 returns (name,father,mother)
+
+; 
+
+-- Q2 returns (name)
+
+;
+
+-- Q3 returns (name)
+
+;
+
+-- Q4 returns (house,name,accession)
+
+;
+
+-- Q5 returns (first_name,popularity)
+
+;
+
+-- Q6 returns (house,seventeenth,eighteenth,nineteenth,twentieth)
+
+; 
+
+-- Q7 returns (father,child,born)
+
+;
+
+-- Q8 returns (monarch,prime_minister)
+
+;
+
